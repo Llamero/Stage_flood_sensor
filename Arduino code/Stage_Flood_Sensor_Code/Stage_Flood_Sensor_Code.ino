26fbbcbbb29a31bba97dc30ae0f5ad0d1fbee528
Llamero <benjamin.smith@berkeley.edu>
2023-06-22 17:47:35 -0700

    better comments

--- Arduino code/Stage_Flood_Sensor_Code/Stage_Flood_Sensor_Code.ino
+++ Arduino code/Stage_Flood_Sensor_Code/Stage_Flood_Sensor_Code.ino
@@ -8,7 +8,7 @@ const uint8_t TONE_REP = 3; //Number ot times to repeat beep per sequence
 const uint16_t TONE_PAUSE = 500; //How long to pause between beep sequences in ms
 const uint8_t BOOT_VOLUME = 10; //Beep volume - 0=off; 128 = full volume; 255 = off;
 const uint8_t ALARM_VOLUME = 128; //Beep volume - 0=off; 128 = full volume; 255 = off;
-const uint16_t THRESHOLD = 60000; //Threshold adc value that triggers the alarm
+const uint16_t THRESHOLD = 60000; //Threshold adc value that triggers the alarm - 0-65472, higher values = more sensitive.
 
 //Global variables
 uint16_t adc; //Stores the recorded adc values
@@ -17,22 +17,26 @@ uint8_t beep; //Stores duration of second phase of beep cycle
 
 void setup() {
   //Configure the digital pins
-  DDRD = B11111100;
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  DDRD = B11111100; //Set alarm pins as outputs
+  pinMode(LED_BUILTIN, OUTPUT); //Set LED as output
+  digitalWrite(LED_BUILTIN, LOW); //Turn off LED
 
   //Clear ADC
   analogRead(A0);
   analogRead(A0);
-  
+
+  //Play boot tone
   playBeep(BOOT_VOLUME);
 }
 
 void loop() {
+  //Measure sensor plate resistivity
   avg = 64;
   adc = 0;
-  while(avg--) adc += analogRead(A0); //Measure sensor plate resistivity
-  if(adc < THRESHOLD){ //If resistivity is below threshold, play alarm
+  while(avg--) adc += analogRead(A0); 
+
+  //If resistivity is below threshold, play alarm
+  if(adc < THRESHOLD){ 
     avg = TONE_REP;
     while(avg--) playBeep(ALARM_VOLUME);
     delay(TONE_PAUSE);
@@ -42,7 +46,7 @@ void loop() {
 void playBeep(uint8_t set_volume){
   adc = TONE_DURATION;
   beep = 255-set_volume;
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH); //Turn on LED
   while(adc--){ //Play tone
     PORTD = B10000000;
     delayMicroseconds(set_volume);
@@ -50,6 +54,6 @@ void playBeep(uint8_t set_volume){
     delayMicroseconds(beep);
   }
   adc = TONE_DURATION; //Pause tone
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, LOW); //Turn off LED
   while(adc--) delayMicroseconds(255);
 }
